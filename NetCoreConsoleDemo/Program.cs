814caac154f513b6d1ec4e3df5f53f9f0b4f8d05
MOZART\Vivaldi <leon.sutedja@gmail.com>
2018-09-08 12:12:31 +1000

    Added json app settings

--- NetCoreConsoleDemo/Program.cs
+++ NetCoreConsoleDemo/Program.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using System.IO;
 using Autofac;
+using Microsoft.Extensions.Configuration;
 
 namespace NetCoreConsoleDemo
 {
     internal class Program
     {
-        private static ICommandHandlerFactory _commandHandlerFactory { get; set; }
+        private static ICommandHandlerFactory CommandHandlerFactory { get; set; }
 
         private static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration["ConnectionString"];
+            Console.WriteLine("Connection String: {0}", connectionString);
+
+            var commandToRun = configuration["CommandToRun"];
+            Console.WriteLine("Command to Run: {0}", commandToRun);
+
+            // Get command handler and run
             AutofacContainer.Initiate();
-            _commandHandlerFactory = (ICommandHandlerFactory)AutofacContainer.Container.Resolve(typeof(ICommandHandlerFactory));
-            var sampleCommandHandler = _commandHandlerFactory.GetCommandHandler<SampleCommand, bool>();
-            var sampleCommand = new SampleCommand()
+            CommandHandlerFactory = (ICommandHandlerFactory)AutofacContainer.Container.Resolve(typeof(ICommandHandlerFactory));
+            var sampleCommandHandler = CommandHandlerFactory.GetCommandHandler<SampleCommand, bool>();
+            var sampleCommand = new SampleCommand
             {
                 Id = 1,
                 AccountNo = "1234578",
